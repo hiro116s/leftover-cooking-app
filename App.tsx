33beaebbe2fa6a4890909c608d26729ca54200d6
Hiroshi Nakahara <hiro116s@gmail.com>
2018-10-27 01:28:03 +0900

    Added TypeScript (Reference: https://qiita.com/uryyyyyyy/items/163027b4fb8606f4cdff, https://qiita.com/mottox2/items/84b318bed9f44359606d)

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const styles: any = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default class App extends React.Component<{}, {}> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start work ing on your app!</Text>
+      </View>
+    );
+  }
+}
